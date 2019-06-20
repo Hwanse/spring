e47eefdbc3502f01c2dcb2a4f01f0312f6422e3e
Kimhs <khsb2012@gmail.com>
2019-06-20 11:10:16 +0900

    spring container scope test
     - singleton
     - prototype

--- src/main/java/kr/or/ddit/board/service/BoardServiceImpl.java
+++ src/main/java/kr/or/ddit/board/service/BoardServiceImpl.java
@@ -3,9 +3,19 @@ package kr.or.ddit.board.service;
 import kr.or.ddit.board.dao.IBoardDao;
 
 public class BoardServiceImpl implements IBoardService{
-
+	
+	// property or field
 	private IBoardDao boardDao;
 
+	
+	public BoardServiceImpl(IBoardDao boardDao) {
+		this.boardDao = boardDao;
+	}
+	
+	public BoardServiceImpl() {
+
+	}
+	
 	@Override
 	public String sayHello() {
 		return boardDao.sayHello();
--- src/test/java/kr/or/ddit/ioc/SpringIocTest.java
+++ src/test/java/kr/or/ddit/ioc/SpringIocTest.java
@@ -1,17 +1,17 @@
 package kr.or.ddit.ioc;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import kr.or.ddit.board.dao.IBoardDao;
 import kr.or.ddit.board.service.IBoardService;
 
 // **실무에서 자주 사용하는 형태
@@ -20,9 +20,39 @@ import kr.or.ddit.board.service.IBoardService;
 @ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-test.xml")
 public class SpringIocTest {
 	
-	// bean을 주입처리하는 어노테이션
+	// field기준 boardService, boardService2  : spring boardSErvice bean (scope=singleton)
+	//			 boardServiceConstructor : spring boardServiceConstructor bean(scope=singleton)
+	
+	//1. boardService, boardService2 : 같아야 한다
+	//2. boardService, boardSErviceConstructor : 같은 클래스에서 만들어진 객체이지만 spring singleton
+	//	  개념에 따라 다른객체
+	//3. boardService2, boardServiceConstructor : 같은 크래래스에서 만들어진 객체이지만 spring singleton
+	//    개념에 따라 다른 객체
+	
+	// bean을 주입처리하는 어노테이션 : @Resource
+	// boardDao : spring boardDao(scope=singleton)
+	// boardDaoPrototype : spring boardDaoPrototype(scope=prototype)
+	// boardDaoPrototype2 : spring boardDaoPrototype(scope=prototype)
+	// 1. boardDao boardDaoPrototype : spring bean id가 다르므르로 다른객체
+	// 2. boardDaoPrototype, boardPrototype2 : 두 객체는 같은 스프링 bean이지만
+	//										 : scope가 prototype이므로 다른 객체이어야한다
+	@Resource(name="boardDao")
+	private IBoardDao boardDao;
+	
+	@Resource(name="boardDaoPrototype")
+	private IBoardDao boardDaoPrototype;
+	
+	@Resource(name="boardDaoPrototype")
+	private IBoardDao boardDaoPrototype2;
+
 	@Resource(name="boardService")
 	private IBoardService boardService;
+	
+	@Resource(name="boardService")
+	private IBoardService boardService2;
+	
+	@Resource(name="boardServiceConstructor")
+	private IBoardService boardServiceConstructor;
 	
 	/**
 	* Method : springIocTest
@@ -42,5 +72,49 @@ public class SpringIocTest {
 		assertEquals("boardDao sayHello", msg);
 		
 	}
+	
+	/**
+	* Method : springSingletonScopeTest
+	* 작성자 : PC14
+	* 변경이력 :
+	* Method 설명 : 스프링 컨테이너의 singleton scope 테스트
+	*/
+	@Test
+	public void springSingletonScopeTest() {
+		/***Given***/
+		//1. boardService, boardService2 : 같아야 한다
+		//2. boardService, boardSErviceConstructor : 같은 클래스에서 만들어진 객체이지만 spring singleton
+		//	  개념에 따라 다른객체
+		//3. boardService2, boardServiceConstructor : 같은 크래래스에서 만들어진 객체이지만 spring singleton
+		//    개념에 따라 다른 객체
 
+		/***When***/
+
+		
+		/***Then***/
+		assertNotNull(boardService);
+		assertNotNull(boardService2);
+		assertNotNull(boardServiceConstructor);
+		assertEquals(boardService, boardService2);
+		assertNotEquals(boardService, boardServiceConstructor);
+		assertNotEquals(boardService2, boardServiceConstructor);
+	}
+
+	
+	@Test
+	public void springPrototypeScopeTest() {
+		/***Given***/
+		
+
+		/***When***/
+
+		
+		/***Then***/
+		assertNotNull(boardDao);
+		assertNotNull(boardDaoPrototype);
+		assertNotNull(boardDaoPrototype2);
+		assertNotEquals(boardDao, boardDaoPrototype);
+		assertNotEquals(boardDaoPrototype, boardDaoPrototype2);
+	}
+	
 }
