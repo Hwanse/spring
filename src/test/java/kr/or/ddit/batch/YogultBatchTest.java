4e83b49075d441b697e6b64ad1b0112fa7640c49
Kimhs <khsb2012@gmail.com>
2019-07-08 13:18:13 +0900

    요구르트 배치 중간 커밋(real)

--- src/test/java/kr/or/ddit/batch/YogultBatchTest.java
+++ src/test/java/kr/or/ddit/batch/YogultBatchTest.java
@@ -1,41 +1,54 @@
 package kr.or.ddit.batch;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:"})
-public class RangerBatchTest {
-
+@ContextConfiguration(locations = {
+									"classpath:kr/or/ddit/config/spring/application-datasource-dev.xml",
+									"classpath:kr/or/ddit/config/spring/application-transaction.xml",
+									"classpath:kr/or/ddit/config/spring/application-batch-dev.xml"
+								  })
+public class YogultBatchTest {
+	
 	@Resource(name = "jobLauncher")
 	private JobLauncher jobLauncher;
 	
-	@Resource(name = "rangerJob")
-	private Job rangerJob;
+	@Resource(name = "yogultJob")
+	private Job yogultJob;
 	
 	/**
 	* Method : rangerBatchTest
-	* 작성자 : PC14
+	* 작성자 : PC10
 	* 변경이력 :
-	* Method 설명 : 레이전스 이름 수정 배치 테스트
+	* @throws Exception
+	* Method 설명 : 레인저스 이름 수정 배치 테스트
 	*/
 	@Test
-	public void rangerBatchTest() {
+	public void yogultBatchTest() throws Exception {
 		/***Given***/
+		String st = "201907";
 		
-
 		/***When***/
-		JobExecution jobExecution= jobLauncher.run(rangerJob, new JobParameters());
+		JobExecution jobExecution =  jobLauncher.run(yogultJob,
+					new JobParametersBuilder().addParameter("ym", new JobParameter(st)).toJobParameters() 
+					);
 		
 		/***Then***/
 		assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
-		
 	}
 
 }
